a498ddb87c318495f7dd2e3731222a0b75f02ca2
Alan Vinicius <alan123vinicius@gmail.com>
2020-11-21 22:00:06 -0300

    Add files via upload

---
+++ somar.php
@@ -0,0 +1,18 @@
+<?php
+
+function Soma ($a,$b){
+    return $a + $b;
+}
+
+function Subtracao ($a,$b){
+    return $a - $b;
+}
+
+function Multiplicacao ($a,$b){
+    return $a * $b;
+}
+
+function Divisao ($a,$b){
+    return $a / $b;
+}
+?>
